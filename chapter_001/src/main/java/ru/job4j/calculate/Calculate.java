b39307e7a9acafb855ec94d575bef826844f75a6
raksmiha <mailto:gorunovka93@gmail.com>
2018-07-23 23:41:50 +0500

    1.3. JavDoc. Code convetion[#4087]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,8 +1,18 @@
 package ru.job4j.calculate;
 
+/**
+* Class Calculate для вывода надписи
+* @author prohorov
+* @since 23.07.2018
+* @version 1
+*/
 public class Calculate {
-
+	
+	/**
+	* Method main.
+	* @param args - args.
+	*/
 	public static void main(String[] srgs) {
 		System.out.println("Hello World!");
-}
+	}
 }
---
+++ chapter_001/src/main/java/ru/job4j/calculate/package-info.java
@@ -0,0 +1,8 @@
+/**
+* Package for calculate task.
+*
+* @author Mihail Prohorov (mailto:gorunovka93@gmail.ru)
+* @version $Id$
+* @since 23.07.2018
+*/
+package ru.job4j.calculate;
