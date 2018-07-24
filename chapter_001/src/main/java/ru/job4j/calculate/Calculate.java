99f571f71bb1c11851021429073a390a7e5e2e93
raksmiha <mailto:gorunovka93@gmail.com>
2018-07-24 22:53:24 +0500

    1.3.1 Maven Plugins [#49546]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -12,7 +12,7 @@ public class Calculate {
 	* Method main.
 	* @param args - args.
 	*/
-	public static void main(String[] srgs) {
+	public static void main(String[] args) {
 		System.out.println("Hello World!");
 	}
 }
