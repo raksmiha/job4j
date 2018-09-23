1863d6eebd9aa82a8d32ac53baa9805fab590f06
raksmiha <mailto:gorunovka93@gmail.com>
2018-09-23 11:14:42 +0500

    3.1. Элементарный калькулятор. [#185]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -8,11 +8,33 @@ package ru.job4j.calculate;
 */
 public class Calculate {
 	/**
+	 * Variable result.
+     */
+	private double result;
+
+	/**
+	 * Method add.
+	 * @param first - first number.
+	 * @param second - second number.
+	 */
+	public void add(double first, double second){
+		this.result = first + second;
+	}
+
+    /**
+     * Method getResult.
+     * @return result.
+     */
+    public double getResult() {
+        return this.result;
+    }
+
+	/**
 	* Method echo.
 	* @param name Your name.
 	* @return Echo plus your name.
 	*/
-	public String echo(String name) {
+	public String echo(String name){
 		return "Echo, echo, echo : " + name;
 	}
 	
@@ -20,7 +42,7 @@ public class Calculate {
 	* Method main.
 	* @param args - args.
 	*/
-	public static void main(String[] args) {
+	public static void main(String[] args){
 		System.out.println("Hello World!");
 	}
 }
--- chapter_001/src/test/java/ru/job4j/calculate/CalculateTest.java
+++ chapter_001/src/test/java/ru/job4j/calculate/CalculateTest.java
@@ -2,7 +2,6 @@ package ru.job4j.calculate;
 
 import org.junit.Test;
 
-
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
