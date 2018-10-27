b7356ddc1829106d3992a5000c156406a06e83b9
raksmiha <mailto:gorunovka93@gmail.com>
2018-10-28 01:45:04 +0500

    5.1. Подсчет суммы чётных чисел в диапазоне [#192]

---
+++ chapter_001/src/main/java/ru/job4j/loop/Counter.java
@@ -0,0 +1,25 @@
+package ru.job4j.loop;
+
+/**
+ * Class Max.
+ * @author Prohorov Mikhail (mailto:gorunovka93@gmail.com)
+ * @since 25.10.2018
+ * @version 1.0
+ */
+class Counter {
+	/**
+	 * Method add for summing even numbers.
+	 * @param start first number.
+	 * @param finish latest number.
+	 * @return sum.
+	 */
+	int add(int start, int finish) {
+		int sum = 0;
+		for (; start <= finish; start++) {
+			if (start % 2 == 0) {
+				sum += start;
+			}
+		}
+		return sum;
+	}
+}
---
+++ chapter_001/src/test/java/ru/job4j/loop/CounterTest.java
@@ -0,0 +1,23 @@
+package ru.job4j.loop;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Test.
+ *
+ * @author Mikhail Prokhorov (mailto:gorunovka93@gmail.com)
+ * @version 1
+ * @since 26.10.2018
+ */
+public class CounterTest {
+	/**
+	 * test method add.
+	 */ @Test
+	public void whenSunEvenNumbersFromOneToTenThenThirty() {
+		Counter count = new Counter();
+		int result = count.add(1, 10);
+		assertThat(result, is(30));
+	}
+}
