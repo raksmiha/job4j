ab121333e94b7c17cd5d35ea6791e203e5239537
raksmiha <mailto:gorunovka93@gmail.com>
2018-11-11 04:31:52 +0500

    5.5. Рефакторинг кода. [#33459]

--- chapter_001/src/main/java/ru/job4j/loop/Paint.java
+++ chapter_001/src/main/java/ru/job4j/loop/Paint.java
@@ -1,5 +1,7 @@
 package ru.job4j.loop;
 
+import java.util.function.BiPredicate;
+
 /**
  * Class Paint.
  * @author Prohorov Mikhail (gorunovka93@gmail.com)
@@ -7,44 +9,30 @@ package ru.job4j.loop;
  * @version 1.0
  */
 public class Paint {
-	public String pyramid(int height) {
-		StringBuilder screen = new StringBuilder();
-		int weight = 2 * height - 1;
-		for (int row = 0; row != height; row++) {
-			for (int column = 0; column != weight; column++) {
-				if (row >= height - column - 1 && row + height - 1 >= column) {
-					screen.append("^");
-				} else {
-					screen.append(" ");
-				}
-			}
-			screen.append(System.lineSeparator());
-		}
-		return screen.toString();
+	public String rightTrl(int height) {
+		return this.loopBy(
+				height,
+				height,
+				(row, column) -> row >= column);
 	}
 
 	public String leftTrl(int height) {
-		StringBuilder screen = new StringBuilder();
-		int weight = height;
-		for (int row = 0; row != height; row++) {
-			for (int column = 0; column != weight; column++) {
-				if (row >= (weight - column - 1)) {
-					screen.append("^");
-				} else {
-					screen.append(" ");
-				}
-			}
-			screen.append(System.lineSeparator());
-		}
-		return screen.toString();
+		return this.loopBy(
+				height,
+				height,
+				(row, column) -> row >= height - column - 1);
 	}
 
-	public String rightTrl(int height) {
+	public String pyramid(int height) {
+		return this.loopBy(height, 2 * height - 1, (row, column) -> row >= height - column - 1 && row + height - 1 >= column);
+	}
+
+	private String loopBy(int height, int weight, BiPredicate<Integer, Integer> predict) {
 		StringBuilder screen = new StringBuilder();
-		int weight = height;
 		for (int row = 0; row != height; row++) {
 			for (int column = 0; column != weight; column++) {
-				if (row >= column) {
+
+				if (predict.test(row, column)) {
 					screen.append("^");
 				} else {
 					screen.append(" ");
