1d7da525d1353004180d636134637ddfb69c2696
raksmiha <mailto:gorunovka93@gmail.com>
2018-11-20 20:49:18 +0500

    6.4. Слово начинается с ... [#41585]

--- chapter_001/src/test/java/ru/job4j/array/ArrayCharTest.java
+++ chapter_001/src/test/java/ru/job4j/array/ArrayCharTest.java
@@ -4,6 +4,12 @@ import org.junit.Test;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
+/**
+ * Class ArrayCharTest for testing class ArrayTest.
+ * @author Prokhorov Mikhail (gorunovka93@gmail.com)
+ * @since 20.11.2018
+ * @version 1.0
+ */
 public class ArrayCharTest {
 	/**
 	 * Method whenStartWithPrefixThenTrue for testing method startWith.
