3b6261f6a0e61eacdf11b614e843bc95cd0f5718
raksmiha <mailto:gorunovka93@gmail.com>
2018-12-01 21:49:43 +0500

    6.7. Квадратный массив заполнен true или false по диагоналям. [#53859]

---
+++ chapter_001/src/main/java/ru/job4j/array/ArrayDuplicate.java
@@ -0,0 +1,36 @@
+package ru.job4j.array;
+
+import java.util.Arrays;
+
+/**
+ * Class ArrayDuplicate for remove quals elements of array.
+ * @author Prokhorov Mikhail (gorunovka93@gmail.com)
+ * @since 28.11.2018
+ * @version 1.0
+ */
+public class ArrayDuplicate {
+	/**
+	 * Method remove for finding quals elements and remove duplicates.
+	 * @param array is our array.
+	 * @return array without duplicates.
+	 */
+	public String[] remove(String[] array) {
+		int checking = 1;
+		int endArray = array.length;
+		String change;
+		for (int i = 0; i < endArray; i++) {
+			for (int j = checking; j < endArray; j++){
+				if (array[i].equals(array[j])) {
+					change = array[j];
+					array[j] = array[endArray - 1];
+					array[endArray - 1] = change;
+					j--;
+					endArray--;
+				}
+			}
+			checking++;
+		}
+		array = Arrays.copyOf(array,endArray);
+		return array;
+	}
+}
---
+++ chapter_001/src/test/java/ru/job4j/array/ArrayDuplicateTest.java
@@ -0,0 +1,24 @@
+package ru.job4j.array;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Class ArrayDuplicateTest for testing class ArrayDuplicate.
+ * @author Prokhorov Mikhail (gorunovka93@gmail.com)
+ * @since 29.11.2018
+ * @version 1.0
+ */
+public class ArrayDuplicateTest {
+	/**
+	 * Method for testing method remove.
+	 */ @Test
+	public void WhenArrayWithDuplicatesThenArreyWithoutDuplicates() {
+		ArrayDuplicate array = new ArrayDuplicate();
+		String[] expectArray = {"Привет", "Мир", "Супер"};
+		String[] resultArray = {"Привет", "Мир", "Привет", "Супер", "Мир"};
+		resultArray = array.remove(resultArray);
+		assertThat(resultArray, is(expectArray));
+	}
+}
